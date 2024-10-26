0bb12b0ec46d7e4fb70f876b80a27fc34f475090
rafaeusn <rafael.c.sene@Hotmail.com>
2024-10-25 23:32:32 -0300

    Add files via upload
    Adicionado:
    Classe Main
    Classe Pokemon
    Subclasses Agua, Fogo, Planta

---
+++ PokeMON/src/Main.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static List<Pokemon> pokemons = new ArrayList<>();
+    private static List<Pokemon> pokemonsInimigos = new ArrayList<>();
+
+    public static void main(String[] args) {
+        inicializarPokemons();
+        inicializarPokemonsInimigos();
+        exibirMenuJogo();
+    }
+
+    private static void inicializarPokemons() {
+        pokemons.add(Fogo.gerarPokemonAleatorio());
+        pokemons.add(Agua.gerarPokemonAleatorio());
+        pokemons.add(Planta.gerarPokemonAleatorio());
+    }
+
+    private static void inicializarPokemonsInimigos() {
+        for (int i = 0; i < 30; i++) {
+            if (i < 10) {
+                pokemonsInimigos.add(Fogo.gerarPokemonAleatorio());
+            } else if (i < 20) {
+                pokemonsInimigos.add(Agua.gerarPokemonAleatorio());
+            } else {
+                pokemonsInimigos.add(Planta.gerarPokemonAleatorio());
+            }
+        }
+    }
+
+    private static void exibirMenuJogo() {
+        while (true) {
+            System.out.println("\n--- Menu de Jogo ---");
+            System.out.println("1. Batalhar");
+            System.out.println("2. Ver Pokémons");
+            System.out.println("3. Sair");
+
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+            switch (escolha) {
+                case 1 -> batalhar();
+                case 2 -> verPokemons();
+                case 3 -> {
+                    System.out.println("Obrigado por jogar!");
+                    System.exit(0);
+                }
+                default -> System.out.println("Escolha inválida.");
+            }
+        }
+    }
+
+    private static void batalhar() {
+        System.out.println("\n--- Escolha seu Pokémon ---");
+        for (int i = 0; i < pokemons.size(); i++) {
+            Pokemon pokemon = pokemons.get(i);
+            System.out.println((i + 1) + ". " + pokemon.getNome() + " - HP: " + pokemon.getHp());
+        }
+
+        int escolhaPokemon = scanner.nextInt() - 1;
+        Pokemon pokemonUsuario = pokemons.get(escolhaPokemon);
+        Pokemon pokemonInimigo = pokemonsInimigos.get(new Random().nextInt(pokemonsInimigos.size()));
+
+        System.out.println("\nVocê escolheu: " + pokemonUsuario.getNome());
+        System.out.println("O Pokémon inimigo é: " + pokemonInimigo.getNome());
+
+        while (pokemonUsuario.getHp() > 0 && pokemonInimigo.getHp() > 0) {
+            int dano = pokemonUsuario.calcularDano(pokemonInimigo);
+            pokemonInimigo.receberDano(dano);
+            System.out.println(pokemonUsuario.getNome() + " ataca e causa " + dano + " de dano em " + pokemonInimigo.getNome() + "!");
+            System.out.println(pokemonInimigo.getNome() + " - HP: " + pokemonInimigo.getHp());
+
+            if (pokemonInimigo.getHp() <= 0) {
+                System.out.println(pokemonInimigo.getNome() + " foi derrotado!");
+                pokemons.add(pokemonInimigo);
+                return;
+            }
+
+            dano = pokemonInimigo.calcularDano(pokemonUsuario);
+            pokemonUsuario.receberDano(dano);
+            System.out.println(pokemonInimigo.getNome() + " ataca e causa " + dano + " de dano em " + pokemonUsuario.getNome() + "!");
+            System.out.println(pokemonUsuario.getNome() + " - HP: " + pokemonUsuario.getHp() + " / " + pokemonUsuario.getHp());
+
+            if (pokemonUsuario.getHp() <= 0) {
+                System.out.println(pokemonUsuario.getNome() + " foi derrotado! Você perdeu a batalha.");
+            }
+        }
+    }
+
+    private static void verPokemons() {
+        System.out.println("\n--- Seus Pokémon ---");
+            for (Pokemon pokemon : pokemons) {
+                System.out.println(pokemon.getNome() + " - HP: " + pokemon.getHp() + " - Tipo: " + pokemon.getTipo());
+            }
+        }
+    }
---
+++ PokeMON/src/Pokemon.java
@@ -0,0 +1,59 @@
+public abstract class Pokemon {
+    protected String nome;
+    protected int hp;
+    protected int ataque;
+    protected String tipo;
+
+    public Pokemon(String nome, int hp, int ataque, String tipo) {
+        this.nome = nome;
+        this.hp = hp;
+        this.ataque = ataque;
+        this.tipo = tipo;
+    }
+
+    public abstract void ataqueEspecial(Pokemon adversario);
+
+    // Método para calcular dano com base nas vantagens de tipo
+    public int calcularDano(Pokemon adversario) {
+        double multiplicador = 1.0;
+
+        switch (this.tipo) {
+            case "Agua" -> {
+                if (adversario.tipo.equals("Fogo")) multiplicador = 2.0;
+                else if (adversario.tipo.equals("Agua")) multiplicador = 1.0;
+                else if (adversario.tipo.equals("Planta")) multiplicador = 0.5;
+            }
+            case "Fogo" -> {
+                if (adversario.tipo.equals("Planta")) multiplicador = 2.0;
+                else if (adversario.tipo.equals("Agua")) multiplicador = 0.5;
+                else if (adversario.tipo.equals("Fogo")) multiplicador = 1.0;
+            }
+            case "Planta" -> {
+                if (adversario.tipo.equals("Agua")) multiplicador = 2.0;
+                else if (adversario.tipo.equals("Fogo")) multiplicador = 0.5;
+                else if (adversario.tipo.equals("Planta")) multiplicador = 1.0;
+            }
+
+        }
+
+        return (int) (this.ataque * multiplicador);
+    }
+
+    // Método para aplicar dano
+    public void receberDano(int dano) {
+        this.hp -= dano;
+        if (this.hp < 0) this.hp = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
